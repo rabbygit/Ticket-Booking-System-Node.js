e2b6da0ba1a4eced564234ae6d57d97c0934f968
rabbygit <rabbyhossain482@gmail.com>
2020-06-16 10:57:35 +0600

    Bus schema added

---
+++ models/Bus.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require("mongoose")
+
+const busSchema = new Schema({
+    merchant: {
+        type: Schema.Types.ObjectId,
+        ref: "Merchant",
+        required: true
+    },
+    departureTrip: {
+        type: Schema.Types.ObjectId,
+        ref: "Trip",
+        required: true
+    },
+    returnTrip: {
+        type: Schema.Types.ObjectId,
+        ref: "Trip",
+        required: true
+    },
+    busNumber: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    busName: {
+        type: String,
+        maxlength: 100
+    },
+    busLogo: String,
+    officeAddress: {
+        type: String,
+        required: true
+    },
+    contactNumber: {
+        type: [String],
+        required: true
+    },
+    busType: {
+        type: String,
+        enum: ["AC", "NON AC"]
+    },
+    totalSeat: Number,
+    seats: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Seat"
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+const Bus = model('Bus', busSchema)
+
+module.exports = Bus;
